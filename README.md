776af50d244bc87861471c548f4e43d3febbc365
Erlis Ibraimi <erloibri1@gmail.com>
2024-07-06 12:16:40 -0400

    added the basics for the home page ui

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ This is a README for the Horizon project.
 - [Contributors](#contributors)
 
 ## Description
-Horizon is a banking app with a finance management dashboard that connects multiple bank accounts, displays real-time transactions, and allows you to transfer money to other users on the platform.
+Horizon is a comprehensive banking app featuring an intuitive finance management dashboard. It seamlessly connects multiple bank accounts, displays real-time transactions, and facilitates instant money transfers to other users within the platform.
 
 ## Technologies
 The project is built using the following technologies:
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox';
 
 const Home = () => {
+  const loggedIn = { firstName: 'E' };
+
   return (
-    <div>Home</div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access and manage your account and finances with ease."
+          />
+
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.12}
+          />
+        </header>
+      </div>
+    </section>
   )
 }
 
