796ba0d98ee5d0bcfc8f99f5d8e53b0b7787e168
Erlis Ibraimi <erloibri1@gmail.com>
2024-07-06 21:47:23 -0400

    implemented both sidebars and complete layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,9 @@
 import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar';
 import TotalBalanceBox from '@/components/TotalBalanceBox';
 
 const Home = () => {
-  const loggedIn = { firstName: 'E' };
+  const loggedIn = { firstName: 'Erlis', lastName: 'Doe', email: 'name@gmail.com' };
 
   return (
     <section className='home'>
@@ -21,7 +22,15 @@ const Home = () => {
             totalCurrentBalance={1250.12}
           />
         </header>
+
+        RECENT TRANSACTIONS
       </div>
+
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 123.12 }, {currentBalance: 193.12}]}
+      />
     </section>
   )
 }
