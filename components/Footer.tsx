9bec28239409337bb5249390f4a3b11ac0331bb9
Erlis Ibraimi <erloibri1@gmail.com>
2024-07-08 22:24:49 -0400

    initalized sentry (an error tracking and performance monitoring platform)

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -16,17 +16,17 @@ const Footer = ({ user, type = "desktop" }: FooterProps) => {
         <footer className='footer'>
             <div className={type === 'mobile' ? 'footer_name-mobile' : 'footer_name'}>
                 <p className='text-xl font-bold text-gray-700'>
-                    {user.name[0]}
+                    {user?.name[0]}
                 </p>
             </div>
 
             <div className={type === 'mobile' ? 'footer_email-mobile' : 'footer_email'}>
                 <h1 className='text-14 truncate font-semibold text-gray-700'>
-                    {user.name}
+                    {user?.name}
                 </h1>
 
                 <p className='text-14 truncate font-normal text-gray-500'>
-                    {user.email}
+                    {user?.email}
                 </p>
             </div>
 
