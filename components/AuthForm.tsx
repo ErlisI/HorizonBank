d79cf370dacc826a6de89ea0d0c064ca6ee56277
Erlis Ibraimi <erloibri1@gmail.com>
2024-07-09 21:32:43 -0400

    added the plaid api and dwolla

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -13,6 +13,7 @@ import { authFormSchema } from '@/lib/utils';
 import { Loader2 } from 'lucide-react';
 import { useRouter } from 'next/navigation';
 import { signIn, signUp } from '@/lib/actions/user.actions';
+import PlaidLink from './PlaidLink';
 
 const AuthForm = ({ type }: { type: string }) => {
     const [user, setUser] = useState(null)
@@ -94,11 +95,11 @@ const AuthForm = ({ type }: { type: string }) => {
                 </div>
             </header>
 
-            {user ? (
+            {/* {user ? ( */}
                 <div className='flex flex-col gap-4'>
-                    {/* PlaidLink */}
+                    <PlaidLink user={user} variant='primary' />
                 </div>
-            ) : (
+            {/* ) : ( */}
                 <>
                     <Form {...form}>
                         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
@@ -205,7 +206,7 @@ const AuthForm = ({ type }: { type: string }) => {
                         </Link>
                     </footer>
                 </>
-            )}
+            {/* )} */}
         </section>
     )
 }
---
+++ components/PlaidLink.tsx
@@ -0,0 +1,60 @@
+'use client';
+
+import { useCallback, useState, useEffect } from "react"
+import { Button } from "./ui/button"
+import { PlaidLinkOnSuccess, PlaidLinkOptions, usePlaidLink } from "react-plaid-link"
+import { useRouter } from "next/navigation"
+import { createLinkToken } from "@/lib/actions/user.actions"
+
+const PlaidLink = ({ user, variant }: PlaidLinkProps) => {
+    const [token, setToken] = useState('')
+    const router = useRouter()
+
+    useEffect(() => {
+        const getLinkToken = async () => {
+            const data = await createLinkToken(user)
+            setToken(data?.linkToken)
+        }
+
+        getLinkToken()
+    }, [user])
+
+    const onSuccess = useCallback<PlaidLinkOnSuccess>(async (public_token: string) => {
+        // await exchangePublicToken({
+        //     public_token,
+        //     user
+        // })
+
+        router.push('/')
+    }, [user])
+
+    const config: PlaidLinkOptions = {
+        token,
+        onSuccess
+    }
+    const { open, ready } = usePlaidLink(config)
+
+    return (
+        <>
+            {variant === 'primary' ? (
+                <Button
+                    onClick={() => open()}
+                    disabled={!ready}
+                    className="plaidlink-primary"
+                >
+                    Connect your bank account
+                </Button>
+            ) : variant === 'ghost' ? (
+                <Button>
+                    Connect your bank account
+                </Button>
+            ) : (
+                <Button>
+                    Connect your bank account
+                </Button>
+            )}
+        </>
+    )
+}
+
+export default PlaidLink
