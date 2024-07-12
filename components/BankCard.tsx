07dad4f045b28bf254741eb77d96afb3b61bc438
Erlis Ibraimi <erloibri1@gmail.com>
2024-07-12 15:41:29 -0400

    finished the bank page in which you can see your cards, open the recent transactions based on the card

--- components/BankCard.tsx
+++ components/BankCard.tsx
@@ -2,9 +2,12 @@ import { formatAmount } from '@/lib/utils'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import Copy from './Copy'
 
 const BankCard = ({ account, userName, showBalance = true }: CreditCardProps) => {
 
+    console.log(account);
+    
     return (
         <div className="flex flex-col">
             <Link href={`/transaction-history/?id=${account.appwriteItemId}`} className="bank-card">
@@ -28,7 +31,7 @@ const BankCard = ({ account, userName, showBalance = true }: CreditCardProps) =>
                             </h2>
                         </div>
                         <p className="text-14 font-semibold tracking-[1.1px] text-white">
-                            ●●●● ●●●● ●●●● <span className="text-16">1234</span>
+                            ●●●● ●●●● ●●●● <span className="text-16">{account?.mask}</span>
                         </p>
                     </article>
                 </div>
@@ -57,6 +60,8 @@ const BankCard = ({ account, userName, showBalance = true }: CreditCardProps) =>
                     className="absolute top-0 left-0"
                 />
             </Link>
+
+            {showBalance && <Copy title={account?.sharaebleId} />}
         </div>
     )
 }
